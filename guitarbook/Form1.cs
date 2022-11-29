ea950ddc3b1167829441b14cc87ca39d082fe6ef
Mohamed Sharif <Mohamed8sharif@gmail.com>
2022-11-29 08:55:55 -0600

    Add project files.

---
+++ guitarbook/Form1.cs
@@ -0,0 +1,36 @@
+using MySql.Data.MySqlClient;
+
+namespace guitarbook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string connetionString = null;
+            MySqlConnection cnn;
+            connetionString = "server=localhost;database=test;uid=root;pwd=password;";
+            cnn = new MySqlConnection(connetionString);
+            try
+            {
+                cnn.Open();
+                MessageBox.Show("Connection Open ! ");
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
+        }
+    }
+}
